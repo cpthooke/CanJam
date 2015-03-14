f11d477d86a24fcd19a63d51367a95ffab78ca8a
Zach Jones <Z_Jones@hotmail.co.uk>
2015-03-14 19:06:31 +0000

    Main Project file
    This is the multi-coloured cube spawning simulator 2015

---
+++ Zach's/Can Jam/Assets/Scripts/GateGenerator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GateGenerator : MonoBehaviour {
+	
+	int colour;
+	public float spawnMin = 0f;
+	public float spawnMax = 0f;
+	public GameObject[] gate;
+
+	// Use this for initialization
+	void Start () 
+	{
+		Invoke ("Spawn", Random.Range (spawnMin, spawnMax));
+	}
+
+	void Spawn () 
+	{
+		Instantiate (gate [Random.Range (0, gate.Length)], transform.position, Quaternion.identity);
+		Invoke ("Spawn", Random.Range (spawnMin, spawnMax));
+	}
+}
---
+++ Zach's/Can Jam/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour 
+{
+	Vector3 playerPosition;
+	public float moveSpeed;
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		playerPosition = this.transform.position;
+		playerPosition.x += moveSpeed;
+		this.transform.position = playerPosition;
+	}
+}
