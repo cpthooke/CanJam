68412513e2f577ce80e64c8e149fa6f6c1785843
Zach Jones <Z_Jones@hotmail.co.uk>
2015-03-14 22:11:39 +0000

    New character sprites

--- Zach's/Can Jam/Assets/Scripts/GateGenerator.cs
+++ Zach's/Can Jam/Assets/Scripts/GateGenerator.cs
@@ -1,9 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GateGenerator : MonoBehaviour {
-	
-	int colour;
+public class GateGenerator : MonoBehaviour 
+{
 	public float spawnMin = 0f;
 	public float spawnMax = 0f;
 	public GameObject[] gate;
@@ -16,7 +15,22 @@ public class GateGenerator : MonoBehaviour {
 
 	void Spawn () 
 	{
-		Instantiate (gate [Random.Range (0, gate.Length)], transform.position, Quaternion.identity);
-		Invoke ("Spawn", Random.Range (spawnMin, spawnMax));
+		int colour;
+		bool inUse = false;
+
+		//do 
+		//{
+			colour = Random.Range (0, gate.Length);
+
+			if (gate[colour]) //Checks for an instance of the generated colour
+				inUse = true;
+			else
+				Debug.Break ();
+				//inUse = false;
+		//} 
+		//while(inUse = true); //Ensures the same colour is not shown twice on the screen
+
+		Instantiate (gate [Random.Range (0, gate.Length)], transform.position, Quaternion.identity); //Randomly selects a gate colour
+		Invoke ("Spawn", Random.Range (spawnMin, spawnMax)); //Calls the spawn method to generate another gate
 	}
 }
