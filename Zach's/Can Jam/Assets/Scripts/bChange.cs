94298ab4fecf15dc164af77aa99ab18ba5571115
Zach Jones <Z_Jones@hotmail.co.uk>
2015-03-15 01:30:48 +0000

    We got dems sprite prefabs in now
    We just need to implement the clicky clicky, switchy switchy buttons now

---
+++ Zach's/Can Jam/Assets/Scripts/bChange.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bChange : MonoBehaviour {
+	public Sprite sprite1;
+	public Sprite sprite2;
+	
+	private SpriteRenderer spriteRenderer;
+	// Use this for initialization
+	void Start () {
+		spriteRenderer = GetComponent<SpriteRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		if ((Input.GetButton ("FireB")) && (Input.GetAxis ("Jump") < 0 || Input.GetAxis ("Jump") > 0))
+//			spriteRenderer.sprite = sprite2;
+//		else
+//			spriteRenderer.sprite = sprite1;
+	}
+}
